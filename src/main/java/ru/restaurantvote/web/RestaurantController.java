782f10a24f8be764d6ef604b885bd3059779e9d5
ValdisWallace <valdisvanwallace@gmail.com>
2019-08-27 07:22:47 +0700

    adjust fix logger

--- src/main/java/ru/restaurantvote/web/RestaurantController.java
+++ src/main/java/ru/restaurantvote/web/RestaurantController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.restaurantvote.model.Restaurant;
-import ru.restaurantvote.repository.RestaurantRepository;
 import ru.restaurantvote.service.RestaurantService;
 
 import java.util.List;
